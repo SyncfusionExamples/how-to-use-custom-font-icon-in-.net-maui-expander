c9db8fabcea8492df7fd2a0a11b18b3c4e24fcb9
JayashreeRavishankar <jayashree.sf3546@gmail.com>
2023-12-20 12:57:48 +0530

    added sample for how-to-use-custom-font-icon-in-.net-maui-expander

---
+++ ExpanderMaui/ExpanderMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpanderMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
